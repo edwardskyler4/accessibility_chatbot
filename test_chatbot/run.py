04a0c2cd06254a1b1f1e9df2a8b0ea1aa3cbaa4f
edwardskyler4 <edwardskyler4@gmail.com>
2025-10-31 13:42:25 -0600

    finished test chatbot (no official testing done)

---
+++ test_chatbot/run.py
@@ -0,0 +1,61 @@
+import json
+import random
+import torch
+import os
+from model import NeuralNet
+from training import bag_of_words, tokenize
+
+if os.name == "nt":
+    INTENTS_FILE = "accessibility_chatbot\\test_chatbot\\intents.json"
+else:
+    INTENTS_FILE = "intents.json"
+
+if os.name == "nt":
+    DATA_FILE = "accessibility_chatbot\\test_chatbot\\model_data.pth"
+else:
+    DATA_FILE = "model_data.pth"
+
+with open(INTENTS_FILE, "r") as f:
+    intents = json.load(f)
+
+data = torch.load(DATA_FILE)
+
+input_size = data["input size"]
+hidden_size = data["hidden size"]
+output_size = data["output size"]
+all_words = data["all words"]
+tags = data["tags"]
+model_state = data["model state"]
+
+model = NeuralNet(input_size, hidden_size, output_size)
+model.load_state_dict(model_state)
+model.eval()
+
+bot_name = "Testy"
+print("Let's talk. Type 'quit' to exit.")
+
+while True:
+    sentence = input("You: ")
+    if sentence.lower() == "quit":
+        print(f"Okay, goodbye!")
+        break
+
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = torch.from_numpy(X).unsqueeze(0)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    print(probs)
+    prob = probs[0][predicted.item()] # type: ignore
+
+    if prob.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent["responses"])}")
+
+    else:
+        print(f"{bot_name}: I don't understand...")
--- test_chatbot/training.py
+++ test_chatbot/training.py
@@ -1,19 +1,131 @@
 import spacy
 import torch
+import torch.nn as nn
 import json
+import os
+from torch.utils.data import Dataset, DataLoader
 from model import NeuralNet
 import numpy as np
 
-nlp = spacy.load("en_web_core_lg")
+nlp = spacy.load("en_core_web_lg")
 
-def bag_of_words(sentence, words):
-    tokenized_sentence = nlp(sentence)
+def stem(word):
+    return nlp(word)
+
+def tokenize(sentence):
+    return nlp(sentence)
+
+def bag_of_words(tokenized_sentence, words):
+    tokenized_sentence = [w.lemma_ for w in tokenized_sentence]
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
         if w in tokenized_sentence:
             bag[idx] = 1.0
     return bag
 
-with open("intents.json", "r") as f:
+if os.name == "nt":
+    INTENTS_FILE = "accessibility_chatbot\\test_chatbot\\intents.json"
+else:
+    INTENTS_FILE = "intents.json"
+
+with open(INTENTS_FILE, "r") as f:
     intents = json.load(f)
 
+def main():
+    all_words = []
+    tags = []
+    xy = []
+
+    for intent in intents["intents"]:
+        tag = intent["tag"]
+        tags.append(tag)
+        for pattern in intent["patterns"]:
+            w = tokenize(pattern)
+            all_words.extend(w)
+            xy.append((w, tag))
+
+    ignore_words = ["?", "!", ".", ","]
+    all_words = [w.lemma_ for w in all_words if w.orth_ not in ignore_words]
+    all_words = sorted(set(all_words))
+    tags = sorted(set(tags))
+
+    X_train = []
+    y_train = []
+
+    for (pattern_sentence, tag) in xy:
+        bag = bag_of_words(pattern_sentence, all_words)
+        X_train.append(bag)
+        y_train.append(tags.index(tag))
+
+
+    class ChatDataset(Dataset):
+        def __init__(self):
+            self.n_samples = len(X_train)
+            self.x_data = X_train
+            self.y_data = y_train
+
+        def __getitem__(self, index):
+            return self.x_data[index], self.y_data[index]
+        
+        def __len__(self):
+            return self.n_samples
+
+
+    # Hyperparameters
+    batch_size = 8
+    hidden_size = 8
+    output_size = len(tags)
+    input_size = len(all_words)
+    learning_rate = 0.01
+    num_epochs = 800
+
+    dataset = ChatDataset()
+    train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+
+    model = NeuralNet(input_size, hidden_size, output_size)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Train loop
+    for epoch in range(num_epochs):
+        for (words, labels) in train_loader:
+            outputs = model(words)
+            train_loss = criterion(outputs, labels)
+
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+        
+        if (epoch + 1) % 100 == 0:
+
+            """
+            I want to add some testing here, but I would also need to add testing data
+            """
+
+            print(f"Epoch: {epoch + 1}/{num_epochs}, Loss: {train_loss.item()}")
+        
+    print(f"Final loss: {train_loss.item()}")
+
+
+    # Save data
+    data = {
+        "model state": model.state_dict(),
+        "input size": input_size,
+        "hidden size": hidden_size,
+        "output size": output_size,
+        "all words": all_words,
+        "tags": tags
+    }
+
+    if os.name == "nt":
+        DATA_FILE = "accessibility_chatbot\\test_chatbot\\model_data.pth"
+    else:
+        DATA_FILE = "model_data.pth"
+
+    torch.save(data, DATA_FILE)
+
+    print(f"Training complete. Data saved to {DATA_FILE}")
+
+if __name__ == "__main__":
+    main()
